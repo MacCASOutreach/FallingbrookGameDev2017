b3549e9895a30ea2c9d9e1777dbecb65041d3cf1
CSchank <chris@schankula.ca>
2017-09-28 11:08:14 -0400

    add GuitarString live example

---
+++ LiveExamples/GuitarString.elm
@@ -0,0 +1,53 @@
+--Copy this into a 1030-1039 notebook
+
+
+module Main exposing (..)
+
+
+myShapes model =
+    [ curve ( 0, 0 ) [ Pull ( wiggleAmount model * sin (10 * model.time), -8 ) ( 0, -42 ) ]
+        |> outlined (solid 1) grey
+    , circle 10
+        |> filled red
+        |> move ( -30, 20 )
+        |> notifyTap RedButton
+    ]
+
+
+wiggleAmount model =
+    if model.string == Wiggle then
+        10
+    else
+        0
+
+
+type GuitarString
+    = Wiggle
+    | NotWiggle
+
+
+change old =
+    case old of
+        NotWiggle ->
+            Wiggle
+
+        Wiggle ->
+            NotWiggle
+
+
+type Msg
+    = Tick Float GetKeyState
+    | RedButton
+
+
+update msg model =
+    case msg of
+        Tick t _ ->
+            { model | time = t }
+
+        RedButton ->
+            { model | string = change model.string }
+
+
+init =
+    { time = 0, string = NotWiggle }
